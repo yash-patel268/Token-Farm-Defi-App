17ff17a2243f2c989e5ddf72227bfa251cebb010
yash-patel268 <yashp2126@gmail.com>
2021-12-30 19:29:28 -0500

    Added constructor to contract

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -5,4 +5,11 @@ import "./DaiToken.sol";
 
 contract TokenFarm{
     string public name = "Dapp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    constructor(DappToken _dappToken, DaiToken _daiToken) public{
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 }
