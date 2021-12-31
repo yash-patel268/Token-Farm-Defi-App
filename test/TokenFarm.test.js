b73b83814a6f2d2ad609d068aa525bb8e3f24f87
yash-patel268 <yashp2126@gmail.com>
2021-12-30 20:15:42 -0500

    Updated test

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -1,4 +1,5 @@
 const { assert } = require('chai')
+const { default: Web3 } = require('web3')
 
 const DappToken = artifacts.require("DappToken")
 const DaiToken = artifacts.require("DaiToken")
@@ -8,11 +9,25 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
+function tokens(n){
+    return web3.utils.toWei(n, 'ether')
+}
+
 contract('TokenFarm', (accounts) => {
-    
+    let daiToken, dappToken, tokenFarm
+
+    before(async () => {
+        //Load tokens for testing
+        daiToken = await DaiToken.new()
+        dappToken = await DappToken.new()
+        tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address)
+
+        //Transfer all Dapp tokens to farm
+        await dappToken.transfer(tokenFarm.address, tokens('1000000'))
+    })
+
     describe('Mock DAI deployment', async () => {
         it('has a name', async() => {
-            let daiToken = await DaiToken.new()
             const name = await daiToken.name()
             assert.equal(name, "Mock DAI Token")
         })
