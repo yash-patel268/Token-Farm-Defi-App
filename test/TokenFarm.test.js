40ddca75fc500b4cf2360434e6c8623a4564f3f4
yash-patel268 <yashp2126@gmail.com>
2021-12-30 20:06:08 -0500

    Added test to check name

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -1,3 +1,5 @@
+const { assert } = require('chai')
+
 const DappToken = artifacts.require("DappToken")
 const DaiToken = artifacts.require("DaiToken")
 const TokenFarm = artifacts.require("TokenFarm")
@@ -5,3 +7,14 @@ const TokenFarm = artifacts.require("TokenFarm")
 require('chai')
     .use(require('chai-as-promised'))
     .should()
+
+contract('TokenFarm', (accounts) => {
+    
+    describe('Mock DAI deployment', async () => {
+        it('has a name', async() => {
+            let daiToken = await DaiToken.new()
+            const name = await daiToken.name()
+            assert.equal(name, "Mock DAI Token")
+        })
+    })
+})
