83eb558c99efe1bd6cfc7f6ad31c3044e3d4add3
yash-patel268 <yashp2126@gmail.com>
2022-01-04 15:44:50 -0500

    Added function to update stake balance

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -8,6 +8,8 @@ contract TokenFarm{
     DappToken public dappToken;
     DaiToken public daiToken;
 
+    mapping(address => uint) public stakingBalance;
+
     constructor(DappToken _dappToken, DaiToken _daiToken) public{
         dappToken = _dappToken;
         daiToken = _daiToken;
@@ -17,5 +19,8 @@ contract TokenFarm{
     function stakeTokens(uint _amount) public {
         //transfer Mock Dai tokens to this contract for staking
         daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        //update the balance of what is staked
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
     }
 }
