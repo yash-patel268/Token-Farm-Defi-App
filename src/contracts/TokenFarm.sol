6e3c29eb56ff172fef9648aa0fe6aab284174a4d
yash-patel268 <yashp2126@gmail.com>
2022-01-04 15:40:59 -0500

    Created token staking function

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -12,4 +12,10 @@ contract TokenFarm{
         dappToken = _dappToken;
         daiToken = _daiToken;
     }
+
+    //Stake tokens with contract
+    function stakeTokens(uint _amount) public {
+        //transfer Mock Dai tokens to this contract for staking
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+    }
 }
