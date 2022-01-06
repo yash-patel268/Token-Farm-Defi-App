784663bd2ce753f6f970d9d31258d2f112b20332
yash-patel268 <yashp2126@gmail.com>
2022-01-05 21:32:11 -0500

    Added table with balances

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,21 @@ import React, { Component } from 'react'
 class Main extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello, World!</h1>
+      <div id="content" className="mt-3">
+        <table className="table table-borderless text-muted text-center">
+            <thead>
+                <tr>
+                    <th scope="col">Staking Balance</th>
+                    <th scope="col">Reward Balance</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{window.web3.utils.fromWei(this.props.stakingBalance, 'Ether')} mDAI</td>
+                    <td>{window.web3.utils.fromWei(this.props.dappTokenBalance, 'Ether')} DAPP</td>
+                </tr>
+            </tbody>
+        </table>
       </div>
     );
   }
