a7007e7c6479d0bed36099ba53c68e0cd1ba6c8f
yash-patel268 <yashp2126@gmail.com>
2022-01-05 23:04:25 -0500

    Added unstakeTokens function

--- src/components/App.js
+++ src/components/App.js
@@ -79,6 +79,13 @@ class App extends Component {
     })
   }
 
+  unstakeTokens = (amount) => {
+    this.setState({ loading: true })
+    this.state.tokenFarm.methods.unstakeTokens().send({ from: this.state.account }).on('transactionHash', (hash) => {
+      this.setState({ loading: false })
+    })
+  }
+
   constructor(props) {
     super(props)
     this.state = {
@@ -103,6 +110,7 @@ class App extends Component {
         dappTokenBalance={this.state.dappTokenBalance}
         stakingBalance={this.state.stakingBalance}
         stakeTokens={this.stakeTokens}
+        unstakeTokens={this.unstakeTokens}
       />
     }
 
